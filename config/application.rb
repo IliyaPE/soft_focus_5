06f1f0f1d67597ee885ce4619c76bc389c7da330
IliyaPE <127507860+IliyaPE@users.noreply.github.com>
2026-02-06 23:31:54 +0800

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Softfocus
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.hosts << /.*\.easypanel\.host/
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
